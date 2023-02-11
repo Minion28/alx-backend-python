557974cb56656155b22c213229dfdc2de10f2d11
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-02-11 11:44:34 +0300

    basicannotations add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    function add returning sum of floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        return the sum of a and b
+    """
+
+    return (a + b)
